6c6c163c2add5a1a79cf5b1d4e1223e680dd318b
Yasin Aydas <115567753+iysz18@users.noreply.github.com.>
2024-11-18 01:01:09 +0100

    Updated the whole code, removed everthing refactored the playersModule.

--- js/game.js
+++ js/game.js
@@ -1,31 +1,17 @@
-// players are hardcoded for the testing purpose and will be replacedwith dynamic playercreation
-const playerModule = (() => {
-    const players = {
-        X: { token: "X" },
-        O: { token: "O" },
-    };
+const playersModule = (() => {
+    const players = { X: "X", O: "O" };
 
-    let currentPlayerTurn = players.X;
+    let currentPlayer = players.X; 
 
-    const getToken = (playerToken) => players[playerToken]?.token;
+    const getCurrentPlayer = () => currentPlayer; 
+    const getPlayerToken = (player) => players[player];
     const switchTurn = () => {
-        currentPlayerTurn = currentPlayerTurn === players.X ? players.O : players.X; 
+        currentPlayer = currentPlayer === players.X ? players.O : players.X;
     };
-    
+
     return {
-        getToken,
-        switchTurn,        
-    }
+        getCurrentPlayer,
+        getPlayerToken,
+        switchTurn,
+    };
 })();
-
-
-
-
-const cellArray = document.querySelectorAll(".cell");
-
-// testing: iterating over the cellArray, adding evetlistener to change textContent to X
-cellArray.forEach(cell => {
-    cell.addEventListener("click", () => {
-        cell.textContent = "X";
-    });
-});
