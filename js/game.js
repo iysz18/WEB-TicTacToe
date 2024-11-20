6c0ebad1afaba894a53ace815f131bdcb6c69434
Yasin Aydas <115567753+iysz18@users.noreply.github.com.>
2024-11-20 15:21:05 +0100

    added the gameboardModule handling the event bindings, marking cells and referecens to the DOM.

--- js/game.js
+++ js/game.js
@@ -16,20 +16,33 @@ const playersModule = (() => {
     };
 })();
 
+// the gameboardModule binds the click event to each cell and
+// holds the variables referecing the DOM
+const gameboardModule = (() => {
+    // store each cell from the gameboard in a nodelist
+    const cells = document.querySelectorAll(".cells"); 
 
-const gameboardModule = () => {
-    // get each cell and store it in a nodelist
-    const cellNodeList = document.querySelectorAll(".cell");
-    cellNodeList.forEach(cell => {
-        cell.addEventListener("click", () => {
-            
-       });
-    });
+    // the logic to mark the clicked cell if possible (empty = place / occupied = alert players)
+    const markCell = (cell, token) => {
+        if (!cells.textContent.trim()) {
+            // if true place the token of the current player
+            cell.textContent = token;
+            return true;
+        } else {
+            alert("Cell is already occupied!");
+            return false;
+        }
+    };    
 
-    // reference to the div "moveLeftDisplay"
-    const moveCounter = document.querySelector(".movesLeftDisplay");
+    // adds to each cell from the cells nodelist the click event and passes the onClickCell function
+    const addEvents = (onCellClick) => {
+        cells.forEach(cell => {
+            cell.addEventListener("click", onCellClick());
+        });
+    };
     
-
-}; 
-
-gameboardModule();
+    return {
+        markCell,
+        addEvents,
+    }
+})();
